9eada3febf95922cefd62508e9abbe5c6d2f67f7
aditraj24 <adityarajykme24@gmail.com>
2025-12-26 16:17:11 +0530

    update axios base URL to use production environment

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const baseURL =
-  import.meta?.env?.VITE_BASE_URL || "http://localhost:8000/api/v1";
+const baseURL = "https://storybook-2.onrender.com";
 const api = axios.create({
   baseURL,
   withCredentials: true, // required for cookies
